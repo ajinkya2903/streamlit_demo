f0790517890561630c5e6274d35c268e4a1b0572
Avinash <avinash.kadam@digitalmain.com>
2020-08-26 10:10:22 +0530

    Streamlit app deployment

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo “\
+[general]\n\
+email = \”ajinkya.mish@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
